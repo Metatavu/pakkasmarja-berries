3fbbc495d114983b27a6fc9ca0673dfbd65bf1b0
Antti Leppä <antti.leppa@metatavu.fi>
2018-02-12 14:14:54 +0200

    Added support for listing and finding contracts via REST

---
+++ plugins/pakkasmarja-berries-rest/service/contacts-service.js
@@ -0,0 +1,62 @@
+/*jshint esversion: 6 */
+/* global __dirname */
+
+(() => {
+  'use strict';
+
+  const AbstractService = require(`${__dirname}/../abstract-service`);
+
+  /**
+   * Abstract base class for Contacts REST service
+   */
+  class AbstractContactsService extends AbstractService {
+
+   /**
+    * Find contact
+    * Finds contact by id
+    *
+    * @param {http.ClientRequest} req client request object
+    * @param {http.ServerResponse} res server response object
+    **/
+    findContact(req, res) {
+      res.status(501).send();
+    }
+
+   /**
+    * Lists contacts
+    * Lists contacts
+    *
+    * @param {http.ClientRequest} req client request object
+    * @param {http.ServerResponse} res server response object
+    **/
+    listContacts(req, res) {
+      res.status(501).send();
+    }
+
+   /**
+    * Update contact
+    * Updates single contact
+    *
+    * @param {http.ClientRequest} req client request object
+    * @param {http.ServerResponse} res server response object
+    **/
+    updateContact(req, res) {
+      res.status(501).send();
+    }
+
+   /**
+    * Registers REST routes
+    *
+    * @param app express object
+    **/
+    register(app) {
+      app.get(`/rest/v1${this.toPath('/contacts/{id}')}`, [ this.restAuth.bind(this) ], this.findContact.bind(this));
+      app.get(`/rest/v1${this.toPath('/contacts')}`, [ this.restAuth.bind(this) ], this.listContacts.bind(this));
+      app.put(`/rest/v1${this.toPath('/contacts/{id}')}`, [ this.restAuth.bind(this) ], this.updateContact.bind(this));
+    }
+  };
+
+  module.exports = AbstractContactsService;
+
+})();
+
---
+++ plugins/pakkasmarja-berries-rest/service/item-groups-service.js
@@ -0,0 +1,50 @@
+/*jshint esversion: 6 */
+/* global __dirname */
+
+(() => {
+  'use strict';
+
+  const AbstractService = require(`${__dirname}/../abstract-service`);
+
+  /**
+   * Abstract base class for ItemGroups REST service
+   */
+  class AbstractItemGroupsService extends AbstractService {
+
+   /**
+    * Find item group
+    * Finds item group by id
+    *
+    * @param {http.ClientRequest} req client request object
+    * @param {http.ServerResponse} res server response object
+    **/
+    findItemGroup(req, res) {
+      res.status(501).send();
+    }
+
+   /**
+    * Lists itemGroups
+    * Lists itemGroups
+    *
+    * @param {http.ClientRequest} req client request object
+    * @param {http.ServerResponse} res server response object
+    **/
+    listItemGroups(req, res) {
+      res.status(501).send();
+    }
+
+   /**
+    * Registers REST routes
+    *
+    * @param app express object
+    **/
+    register(app) {
+      app.get(`/rest/v1${this.toPath('/itemGroups/{id}')}`, [ this.restAuth.bind(this) ], this.findItemGroup.bind(this));
+      app.get(`/rest/v1${this.toPath('/itemGroups')}`, [ this.restAuth.bind(this) ], this.listItemGroups.bind(this));
+    }
+  };
+
+  module.exports = AbstractItemGroupsService;
+
+})();
+
