446661ad01875f49e1bc3df7933ddac812ecd256
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-03-07 15:01:38 +0200

    Added docker support

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ WORKDIR /opt/wkhtmltopdf
 RUN curl -sSL "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz"|tar -xvJ
 WORKDIR /usr/src/pakkasmarja
 COPY package*.json ./
+COPY docker-default-config.json ./default-config.json
 RUN npm install
 COPY . .
 EXPOSE 3000
--- app.js
+++ app.js
@@ -17,7 +17,11 @@
   const SequelizeStore = require("connect-session-sequelize")(session.Store);
   
   config
-    .env()
+    .env({
+      separator: "_",
+      lowerCase: true,
+      parseValues: true
+    })
     .file({file: __dirname + "/config.json"})
     .defaults(require( __dirname + "/default-config.json"));
    
@@ -53,9 +57,12 @@
     const clusterMessages = architectApp.getService("pakkasmarja-berries-cluster-messages");
     const logger = architectApp.getService("logger");
     
-    shadyWorker.start(config.get("server-group"), options.getOption("port"), options.getOption("host"));
+    const port = options.getOption("port") || 3000;
+    const host = options.getOption("host") || "localhost";
+    
+    shadyWorker.start(config.get("server-group"), port, host);
 
-    const port = options.getOption("port");
+    
     const app = express();
     const httpServer = http.createServer(app);
 
