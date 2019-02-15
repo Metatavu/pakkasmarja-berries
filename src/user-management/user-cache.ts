33426e04608a1e10cd9d146ef57134c1a536e216
Antti Leppä <antti.leppa@metatavu.fi>
2019-02-15 08:05:36 +0200

    Travis

--- src/user-management/user-cache.ts
+++ src/user-management/user-cache.ts
@@ -1,7 +1,5 @@
 import * as redis from "redis";
-import * as Bluebird from "bluebird";
-
-Bluebird.promisifyAll(redis);
+import { promisify } from "util";
 
 /**
  * Redis storage for users
@@ -30,7 +28,8 @@ export default class UserCache {
   async get(userId: string) {
     console.log("cache 1", userId);
 
-    const value = await (this.client as any).getAsync(this.getKey(userId));
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(this.getKey(userId));
     console.log("cache 2", value);
 
     if (value) {
@@ -58,7 +57,8 @@ export default class UserCache {
    */
   async set(userId: string, user: any) {
     const now = (new Date()).getTime();
-    return await (this.client as any).setAsync(this.getKey(userId), JSON.stringify({
+    const setAsync = promisify(this.client.set).bind(this.client);
+    return await setAsync(this.getKey(userId), JSON.stringify({
       user: user,
       expires: now + this.expireTime
     }));
@@ -71,7 +71,8 @@ export default class UserCache {
    * @returns {Promise} promise
    */
   async unset(userId: string) {   
-    return await (this.client as any).delAsync(this.getKey(userId));
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return await delAsync(this.getKey(userId));
   }
 
   /**
