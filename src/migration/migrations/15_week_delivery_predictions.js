d545c1d841abe397a0a6fa266872679e1adac45f
Ville Koivukangas <ville.koivukangas12@gmail.com>
2019-03-20 08:06:37 +0200

    Removed duplicated key

--- src/migration/migrations/15_week_delivery_predictions.js
+++ src/migration/migrations/15_week_delivery_predictions.js
@@ -7,7 +7,7 @@
     up: async (query, Sequelize) => {
       
       await query.createTable("WeekDeliveryPredictions", {
-        id: { type: Sequelize.UUID, primaryKey: true, validate: { notNull: true }, allowNull: false, validate: { isUUID: 4 } },
+        id: { type: Sequelize.UUID, primaryKey: true, allowNull: false, validate: { isUUID: 4 } },
         itemGroupId: { type: Sequelize.BIGINT, allowNull: false, references: { model: "ItemGroups", key: "id" } },
         userId: { type: Sequelize.STRING(191), allowNull: false },
         amount: { type: Sequelize.BIGINT, allowNull: false },
