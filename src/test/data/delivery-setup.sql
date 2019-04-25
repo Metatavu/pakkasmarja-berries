e61994b911ee191e82e98dbdb8c68e574f0b5c17
Antti Leppä <antti.leppa@metatavu.fi>
2019-04-25 18:36:30 +0300

    Tests

--- src/test/data/delivery-setup.sql
+++ src/test/data/delivery-setup.sql
@@ -10,6 +10,12 @@ VALUES
   ('b1590990-4bbb-11e9-8646-d663bd873d93', 1005, 'Product 1', 100, 10, 'KG', 'SAP1', NOW(), NOW()),
   ('b1590dd2-4bbb-11e9-8646-d663bd873d93', 2005, 'Product 2', 120, 20, 'KG', 'SAP2', NOW(), NOW());
 
+INSERT INTO
+  ProductPrices (id, productId, unit, price, createdAt, updatedAt)
+VALUES
+  ('6a8ef30b-f066-41bc-9efe-5816b61a2b04', 'b1590990-4bbb-11e9-8646-d663bd873d93', 'e / kg ALV 0%', 100, NOW(), NOW())
+  ('80121ca0-1a7e-4135-823c-a55a9b85f463', 'b1590dd2-4bbb-11e9-8646-d663bd873d93', 'e / kg ALV 0%', 200, NOW(), NOW());
+
 INSERT INTO 
     DeliveryPlaces (externalId, sapId, name, createdAt, updatedAt)
 VALUES 
