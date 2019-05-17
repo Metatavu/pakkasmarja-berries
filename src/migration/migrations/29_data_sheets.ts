8045078a5ee0ab32eef459bc8ca3fac8c9d6d787
Antti Leppä <antti.leppa@metatavu.fi>
2019-05-17 11:39:34 +0300

    Added realm role migration script

--- src/migration/migrations/29_data_sheets.ts
+++ src/migration/migrations/29_data_sheets.ts
@@ -1,10 +1,16 @@
 import * as Sequelize from "sequelize";
 import { QueryInterface } from "sequelize";
+import userManagement from "../../user-management";
+import ApplicationRoles from "src/rest/application-roles";
 
 module.exports = {
 
   up: async (query: QueryInterface) => {
-    
+    const role = await userManagement.findRealmRole(ApplicationRoles.MANAGE_DATA_SHEETS);
+    if (!role) {
+      userManagement.createRealmRole(ApplicationRoles.MANAGE_DATA_SHEETS);
+    }
+
     await query.createTable("DataSheets", {
       id: { type: Sequelize.UUID, primaryKey: true, allowNull: false },
       name: { type: Sequelize.STRING(191), allowNull: false },
