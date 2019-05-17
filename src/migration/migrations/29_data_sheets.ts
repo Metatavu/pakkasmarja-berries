5cc38ce82394235222fd8e0b7f701f4ee7610a23
Antti Leppä <antti.leppa@metatavu.fi>
2019-05-17 14:26:22 +0300

    Tests

--- src/migration/migrations/29_data_sheets.ts
+++ src/migration/migrations/29_data_sheets.ts
@@ -1,13 +1,9 @@
 import * as Sequelize from "sequelize";
 import { QueryInterface } from "sequelize";
-import userManagement from "../../user-management";
-import ApplicationRoles from "../../rest/application-roles";
 
 module.exports = {
 
   up: async (query: QueryInterface) => {
-    await userManagement.createRealmRole(ApplicationRoles.MANAGE_DATA_SHEETS);
-
     await query.createTable("DataSheets", {
       id: { type: Sequelize.UUID, primaryKey: true, allowNull: false },
       name: { type: Sequelize.STRING(191), allowNull: false },
