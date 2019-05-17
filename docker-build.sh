282d365ed63bf666c952db5f1bba4f5269fab74a
Antti Leppä <antti.leppa@metatavu.fi>
2019-05-17 15:29:47 +0300

    Bumped version

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 rm -fR dist
 npm install && grunt && npm run build
 docker build -t pakkasmarja-server .
-docker tag $(docker images -q pakkasmarja-server) metatavu/pakkasmarja-server:3.0.2
+docker tag $(docker images -q pakkasmarja-server) metatavu/pakkasmarja-server:3.0.3
 docker push metatavu/pakkasmarja-server
