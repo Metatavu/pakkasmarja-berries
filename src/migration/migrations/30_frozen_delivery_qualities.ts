caa8ecf943fce3590af771112b96bfc7210d71c8
Antti Leppä <antti.leppa@metatavu.fi>
2019-06-03 11:38:03 +0300

    Migrations

--- src/migration/migrations/30_frozen_delivery_qualities.ts
+++ src/migration/migrations/30_frozen_delivery_qualities.ts
@@ -4,7 +4,7 @@ import { QueryInterface } from "sequelize";
 module.exports = {
 
   up: async (query: QueryInterface) => {
-    await query.sequelize.query("INSERT INTO DeliveryQualities (id, itemGroupCategory, name, priceBonus, createdAt, modifiedAt) VALUES ('19955d57-2a9f-4948-8df1-9da4c30f1677', 'FROZEN', 'Perus', '0', NOW(), NOW())");
+    await query.sequelize.query("INSERT INTO DeliveryQualities (id, itemGroupCategory, name, priceBonus, createdAt, updatedAt) VALUES ('19955d57-2a9f-4948-8df1-9da4c30f1677', 'FROZEN', 'Perus', '0', NOW(), NOW())");
   }
 
 };
