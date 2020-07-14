4fe3b764200df7778f1ed7603941dc4634d287c3
Antti Leppä <antti.leppa@metatavu.fi>
2020-07-14 20:01:48 +0300

    Removed deprecated keycloak scripts

--- scripts/export-keycloak-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. scripts/keycloak-version.sh
-
-$KEYCLOAK/bin/standalone.sh -Dkeycloak.migration.action=export -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/tmp/keycloak.conf
