6fa4af8d90bde455817caeaffcf2fc1cb5c298de
Antti Leppä <antti.leppa@metatavu.fi>
2020-07-14 20:13:29 +0300

    Fixed opening hour tests

--- src/test/opening-hours-test.ts
+++ src/test/opening-hours-test.ts
@@ -4,56 +4,95 @@ import auth from "./auth";
 import database from "./database";
 
 const testDataDir = `${__dirname}/../../src/test/data/`;
-const openingHoursExpectedData = require(`${testDataDir}/opening-hours.json`);
-
-test("Test listing opening hours", async (t) => {
-  await database.executeFile(testDataDir, "opening-hours-setup.sql");
-
-  return request("http://localhost:3002")
-    .get(`/rest/v1/openingHours/bad02318-1a44-11e8-87a4-c7808d590a07?until=${new Date().toISOString()}`)
-    .set("Authorization", `Bearer ${await auth.getTokenUser1()}`)
-    .set("Accept", "application/json")
-    .expect(200)
-    .then(async response => {
-      t.comment("response received.");
-      t.equal(response.body.periods.length, 2, "Periods length match");
-      t.equal(response.body.exceptions.length, 2, "Exceptions length match");
-
-      t.comment(response.body.periods[0]);
-      t.comment(response.body.periods[1]);
-      t.comment(response.body.exceptions[0]);
-      t.comment(response.body.exceptions[1]);
-
-      await database.executeFile(testDataDir, "opening-hours-teardown.sql");
-    });
-});
 
 test("Test listing opening hour periods", async (t) => {
   await database.executeFile(testDataDir, "opening-hours-setup.sql");
 
   return request("http://localhost:3002")
-    .get("/rest/v1/openingHours/bad02318-1a44-11e8-87a4-c7808d590a07/periods")
+    .get(`/rest/v1/openingHours/bad02318-1a44-11e8-87a4-c7808d590a07/periods?rangeStart=2020-01-02&rangeEnd=2020-02-05`)
+    .set("Authorization", `Bearer ${await auth.getTokenUser1()}`)
     .set("Accept", "application/json")
     .expect(200)
     .then(async response => {
-      const { periods } = response.body;
-      t.equal(periods.length, 2, "Periods length match");
+      t.comment("response received.");
 
-      t.comment(response.body.periods[0]);
-      t.comment(response.body.periods[1]);
+      t.equal(response.body.length, 2, "Periods length match");
+      t.equal(response.body[1].beginDate, '2020-01-01T00:00:00.000Z', "First period begin date");
+      t.equal(response.body[1].endDate, '2020-01-31T00:00:00.000Z', "First period begin date");
 
       await database.executeFile(testDataDir, "opening-hours-teardown.sql");
     });
 });
 
-// test("Test finding opening hour period", async (t) => {
-//   await database.executeFile(testDataDir, "opening-hours-setup.sql");
+test("Test listing opening hour exceptions", async (t) => {
+  await database.executeFile(testDataDir, "opening-hours-setup.sql");
 
-//   return request("http://localhost:3002")
-//   .get("/rest/v1/deliveryPlaces/440ba119-39c7-408e-ad51-ad946fb61e08")
-//   .set("Accept", "application/json")
-//   .expect(200)
-//   .then(async response => {
-//     await database.executeFile(testDataDir, "opening-hours-teardown.sql");
-//   });
-// });
+  return request("http://localhost:3002")
+    .get(`/rest/v1/openingHours/bad02318-1a44-11e8-87a4-c7808d590a07/exceptions`)
+    .set("Authorization", `Bearer ${await auth.getTokenUser1()}`)
+    .set("Accept", "application/json")
+    .expect(200)
+    .then(async response => {
+      t.comment("response received.");
+      t.equal(response.body.length, 2, "Exceptions length match");
+      t.equal(response.body[0].exceptionDate, '2020-01-15', "First exception date");
+
+      await database.executeFile(testDataDir, "opening-hours-teardown.sql");
+    });
+});
+
+test("Test listing all opening hour periods", async (t) => {
+  await database.executeFile(testDataDir, "opening-hours-setup.sql");
+
+  return request("http://localhost:3002")
+    .get("/rest/v1/openingHours/bad02318-1a44-11e8-87a4-c7808d590a07/periods")
+    .set("Authorization", `Bearer ${await auth.getTokenUser1()}`)
+    .set("Accept", "application/json")
+    .expect(200)
+    .then(async response => {
+      const periods = response.body;
+      t.equal(periods.length, 2, "Periods length match");
+
+      t.equal(periods[1].beginDate, '2020-01-01T00:00:00.000Z', "First period begin date");
+      t.equal(periods[1].endDate, '2020-01-31T00:00:00.000Z', "First period end date");
+
+      await database.executeFile(testDataDir, "opening-hours-teardown.sql");
+    });
+});
+
+test("Test finding last active opening hour", async (t) => {
+  await database.executeFile(testDataDir, "opening-hours-setup.sql");
+
+  return request("http://localhost:3002")
+    .get("/rest/v1/openingHours/bad02318-1a44-11e8-87a4-c7808d590a07/periods")
+    .set("Authorization", `Bearer ${await auth.getTokenUser1()}`)
+    .set("Accept", "application/json")
+    .expect(200)
+    .then(async response => {
+      const periods = response.body;
+      t.equal(periods.length, 2, "Periods length match");
+
+      t.equal(periods[1].beginDate, '2020-01-01T00:00:00.000Z', "First period begin date");
+      t.equal(periods[1].endDate, '2020-01-31T00:00:00.000Z', "First period end date");
+
+      await database.executeFile(testDataDir, "opening-hours-teardown.sql");
+    });
+});
+
+test("Test finding opening hour period", async (t) => {
+  await database.executeFile(testDataDir, "opening-hours-setup.sql");
+
+  return request("http://localhost:3002")
+    .get("/rest/v1/openingHours/bad02318-1a44-11e8-87a4-c7808d590a07/periods?maxResults=1")
+    .set("Authorization", `Bearer ${await auth.getTokenUser1()}`)
+    .set("Accept", "application/json")
+    .expect(200)
+    .then(async response => {
+      const periods = response.body;
+      t.equal(periods.length, 1, "Periods length match");
+
+      t.equal(periods[0].beginDate, '2020-02-01T00:00:00.000Z', "Last active period begin date");
+      t.equal(periods[0].endDate, '2020-02-15T00:00:00.000Z', "Last active end date");
+      await database.executeFile(testDataDir, "opening-hours-teardown.sql");
+    });
+});
