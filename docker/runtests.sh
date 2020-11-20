3d7daffbad3e94c6b564c9b7287113790545e652
Antti Leppä <antti.leppa@metatavu.fi>
2020-11-20 06:48:48 +0200

    Tests

--- docker/runtests.sh
+++ docker/runtests.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 
-HOST=http://localhost:3002
+npm run build
+
+HOST=http://pakkasmarja-api:3002
 echo "Waiting for $HOST"
 until $(curl --output /dev/null --silent --head --fail $HOST); do
   echo '.'
   sleep 5
 done
+
+npm run test
---
+++ tests.Dockerfile
@@ -0,0 +1,10 @@
+FROM node:dubnium
+
+WORKDIR /usr/src/pakkasmarja
+ADD . .
+COPY ../package*.json ./
+RUN npm install
+
+ADD docker/runtests.sh /opt/docker/entrypoint.sh
+RUN chmod a+x /opt/docker/entrypoint.sh
+CMD "/opt/docker/entrypoint.sh"
