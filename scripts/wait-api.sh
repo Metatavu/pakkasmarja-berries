d92e6696a80c468481d02082ad1678aa5dd8017c
Antti Leppä <antti.leppa@metatavu.fi>
2021-02-24 12:18:24 +0200

    cb

--- scripts/wait-api.sh
+++ scripts/wait-api.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-until $(curl --output /dev/null --silent --head --fail http://pakkasmarja-api:3002); do
-  curl --head http://pakkasmarja-api:3002
+until $(curl --output /dev/null --silent --head --fail http://pakkasmarja-api:3002/system/ping); do
+  curl --head http://pakkasmarja-api:3002/system/ping
   # echo '.'
   sleep 5
 done
