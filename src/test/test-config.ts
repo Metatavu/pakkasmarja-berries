8f451695030a47f2fd4a19c7ef3de2d08247aec1
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2021-05-05 14:09:09 +0300

    Fixed env name

--- src/test/test-config.ts
+++ src/test/test-config.ts
@@ -1,4 +1,4 @@
 export default {
   HOST: process.env.TEST_HOST || "http://localhost:3002",
-  SAP_TEST_CLIENT_HOST: process.env.SAP_TEST_CLIENT_URL || "http://localhost:1234"
+  SAP_TEST_CLIENT_HOST: process.env.SAP_TEST_CLIENT_HOST || "http://localhost:1234"
 }
