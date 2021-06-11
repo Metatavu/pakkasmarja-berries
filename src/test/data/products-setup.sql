eb7dee3b5c32e0a94436c6642c3c787a5c61aaba
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2021-06-11 09:05:25 +0300

    Fixed tests for create SAP contract

---
+++ src/test/data/products-setup.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+  Products (id, itemGroupId, name, units, unitSize, unitName, sapItemCode, active, createdAt, updatedAt)
+VALUES
+  ('b1590990-4bbb-11e9-8646-d663bd873d93', 1000, 'Product 1', 5, 0.2, 'ltk', 'SAP1', true, NOW(), NOW()),
+  ('b1590dd2-4bbb-11e9-8646-d663bd873d93', 2000, 'Product 2', 5, 0.2, 'ltk', 'SAP2', false, NOW(), NOW());
