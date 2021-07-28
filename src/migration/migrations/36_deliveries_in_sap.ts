dd4f822b59ce1c0ff35ffe1ac84d0e2e0007b7da
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2021-07-28 12:00:53 +0300

    Update src/migration/migrations/36_deliveries_in_sap.ts

--- src/migration/migrations/36_deliveries_in_sap.ts
+++ src/migration/migrations/36_deliveries_in_sap.ts
@@ -2,7 +2,6 @@ import * as Sequelize from "sequelize";
 import { QueryInterface } from "sequelize";
 
 module.exports = {
-
   up: async (query: QueryInterface) => {
     await query.addColumn("Deliveries", "inSap", { type: Sequelize.BOOLEAN, allowNull: false });
     await query.sequelize.query("UPDATE Deliveries SET inSap = false");
