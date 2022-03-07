d7d57b82bcec4dc6ad0086de22d5ef3af3f2efe7
Antti Leppä <antti.leppa@metatavu.fi>
2022-03-07 16:59:05 +0200

    Added scripts for importing production databases

---
+++ scripts/import-prod-pm-database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting docker container for MySQL..."
+docker-compose -f docker-compose-prod.yml up -d mysql
+
+sleep 5
+
+CONTAINER_ID=$(docker ps -q --filter name=pakkasmarja-berries-mysql-1)
+echo "MySQL container started with ID: $CONTAINER_ID"
+
+docker cp ../prod-data/db_dumps/pm.sql $CONTAINER_ID:/tmp/pm.sql
+echo "Copied database dump for Pakkasmarja"
+
+echo "Creating databases with dump data..."
+docker exec $CONTAINER_ID  mysql -uroot -proot -e 'DROP DATABASE IF EXISTS `pm`; CREATE DATABASE `pm` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */; USE pm; source /tmp/pm.sql; commit;'
+
+docker-compose -f docker-compose-prod.yml down
