2eea5c9a404d41428e96d9a18e4d58c446b12d20
Antti Leppä <antti.leppa@metatavu.fi>
2022-03-08 10:02:54 +0200

    Fixed issues with images in prod version

--- scripts/import-prod-pm-database.sh
+++ scripts/import-prod-pm-database.sh
@@ -5,13 +5,14 @@ docker-compose -f docker-compose-prod.yml up -d mysql
 
 sleep 5
 
-CONTAINER_ID=$(docker ps -q --filter name=pakkasmarja-berries-mysql-1)
+CONTAINER_ID=pakkasmarja-berries-mysql-1
 echo "MySQL container started with ID: $CONTAINER_ID"
 
 docker cp ../prod-data/db_dumps/pm.sql $CONTAINER_ID:/tmp/pm.sql
+docker cp ../prod-data/local.sql $CONTAINER_ID:/tmp/local.sql
 echo "Copied database dump for Pakkasmarja"
 
 echo "Creating databases with dump data..."
-docker exec $CONTAINER_ID  mysql -uroot -proot -e 'DROP DATABASE IF EXISTS `pm`; CREATE DATABASE `pm` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */; USE pm; source /tmp/pm.sql; commit;'
+docker exec $CONTAINER_ID  mysql -uroot -proot -e 'DROP DATABASE IF EXISTS `pm`; CREATE DATABASE `pm` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */; USE pm; source /tmp/pm.sql; source /tmp/local.sql; commit;'
 
 docker-compose -f docker-compose-prod.yml down
