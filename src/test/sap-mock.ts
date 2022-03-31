847e380ed5f30766f45d2ced667eb75f62930d54
Antti Leppä <antti.leppa@metatavu.fi>
2022-03-31 19:41:34 +0300

    Added SAP mocker for tests

---
+++ src/test/sap-mock.ts
@@ -0,0 +1,51 @@
+import { EntriesApi } from "../generated/odata-client/api";
+
+const testDataDir = `${__dirname}/../../src/test/data/`;
+const businessPartners = require(`${testDataDir}/erp/business-partners.json`);
+
+/**
+ * Mock client for odata-mock
+ */
+export default new class SapMock {
+
+  /**
+   * Mock business partner
+   * 
+   * @param id id
+   * @returns mocked entry
+   */
+  public mockBusinessPartner = async (id: string) => {
+    try {
+      const data = JSON.stringify(businessPartners[id]);
+
+      console.log({
+        data: data
+      });
+
+      return await this.getEntriesApi().createEntry({
+        data: data,
+        name: "BusinessPartner"
+      });
+    } catch (e) {
+      console.error("SAP Mock request failed", e);
+      return null;
+    }
+  };
+
+  /**
+   * Delete all mocks
+   */
+  public deleteMocks = () => {
+    return this.getEntriesApi().deleteEntries();
+  }
+
+  /**
+   * Returns entries API
+   * 
+   * @returns entries API
+   */
+  private getEntriesApi = () => {
+    return new EntriesApi("http://sap-mock:8080");
+  }
+  
+};
