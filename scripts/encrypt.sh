7400f3f4e5b02b6c1c515f61af8abb5c8e205e14
Antti Leppä <antti.leppa@metatavu.fi>
2022-03-31 07:24:34 +0300

    Updated encrypted

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-tar -czf - ./test/sap-api-wiremock | openssl enc -pbkdf2 -aes-256-cbc -iter $ENC_ITER -md sha512 -pass env:ENC_PASS -out secured.tar.gz
+tar -czf - ./test/sap-api-wiremock ./test-volumes/odata-mock | openssl enc -pbkdf2 -aes-256-cbc -iter $ENC_ITER -md sha512 -pass env:ENC_PASS -out secured.tar.gz
