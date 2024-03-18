9ca644837edfc583598158b1f8d210c111b1268a
Antti Leppä <antti.leppa@metatavu.fi>
2024-03-18 12:38:07 +0200

    Added generated keycloak admin client version

---
+++ src/generated/keycloak-admin-client/apis/index.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AttackDetectionApi';
+export * from './AuthenticationManagementApi';
+export * from './ClientAttributeCertificateApi';
+export * from './ClientInitialAccessApi';
+export * from './ClientRegistrationPolicyApi';
+export * from './ClientRoleMappingsApi';
+export * from './ClientScopesApi';
+export * from './ClientsApi';
+export * from './ComponentApi';
+export * from './DefaultApi';
+export * from './GroupsApi';
+export * from './IdentityProvidersApi';
+export * from './KeyApi';
+export * from './ProtocolMappersApi';
+export * from './RealmsAdminApi';
+export * from './RoleMapperApi';
+export * from './RolesApi';
+export * from './RolesByIDApi';
+export * from './ScopeMappingsApi';
+export * from './UsersApi';
